6a18f86bb916252dbca9b8718828e424b1737f19
lvd01091995 <46308605+lvd01091995@users.noreply.github.com>
2021-10-16 22:13:01 +0700

    firt

---
+++ assets/Main.ts
@@ -0,0 +1,44 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.ScrollView)
+    scr: cc.ScrollView = null;
+
+    // @property
+    // text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+    
+
+    onLoad () {
+        switch(cc.sys.os){
+            case cc.sys.OS_WINDOWS:
+                window.location =  "https://caxeng.win";
+                break;
+            case cc.sys.OS_IOS:
+                window.location =  "https://caxeng.win";
+                break;
+        }
+    }
+    onClickTaiApp(){
+        window.open('https://tai.caxeng.win/Caxeng.apk','_blank');
+    }
+    onClickHuongDan(){
+        this.scr.scrollToPercentVertical(0.18,0.3);
+    }
+
+    // start () {
+
+    // }
+
+    // update (dt) {}
+}
